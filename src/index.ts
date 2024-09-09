e9bbf036edd42040cf82983c29b6a43550feab90
gageracer <gageracer@gmail.com>
2024-09-09 14:50:41 -0700

    printing link

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
+	console.log(`Click here to check the portal: http://localhost:${PORT}`);
 });
